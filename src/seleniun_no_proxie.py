ac9ab47410094cc217093e8884c16427437080ef
Jose maria Moyano <josemariamoyano@MacBook-Pro-de-Jose.local>
2022-04-09 14:13:31 +0200

    added scraped repo

--- src/seleniun_no_proxie.py
+++ src/seleniun_no_proxie.py
@@ -26,7 +26,7 @@ def chromedriver_proxie(PROXY):
 
 def chromedriver_():
         wd = webdriver.Chrome()
-        wd.implicitly_wait(1)
+        wd.implicitly_wait(10)
         return wd
 
 def read_proxies(file):
@@ -36,20 +36,14 @@ def read_proxies(file):
 
 if __name__ == "__main__":
 
-        # wd = chromedriver_()
-
+        wd = chromedriver_()
         url = "https://suchen.mobile.de/fahrzeuge/search.html?dam=0&ecol=BLACK&ecol=GREY&fr=2017%3A&isSearchRequest=true&ml=%3A125000&ms=3500%3B6%3B%3B%3B&od=up&p=10000%3A20000&ref=srp&refId=2804e431-ddff-3817-0f5f-0b62cf3f7222&s=Car&sb=p&sfmr=false&vc=Car?lang=en"
-        url = "https://www.coches.net/segunda-mano/?MakeId=7&MaxKms=140000&MaxYear=2015&MinPrice=10000&MinYear=2015&ModelId=944"
-        proxies = read_proxies("./proxies.txt")
-        for proxie in proxies:
-                wd = chromedriver_proxie(proxie)
-                # wd = chromedriver_()
-                wd.get(url)
-                time.sleep(3)
-                html = wd.page_source
-                soup = BeautifulSoup(html, "html.parser")
+        url = "https://www.flexicar.es/coches-segunda-mano/"
+        wd.get(url)
+        html = wd.page_source
+        soup = BeautifulSoup(html, "html.parser")
                 
-                if 'mobile.de' in url:
+        if 'mobile.de' in url:
                         bloques = soup.select('hitcounters')
 
                         for link in soup.find_all('script'):
@@ -57,23 +51,6 @@ if __name__ == "__main__":
                                         json = link
 
                         string_json = json.get_text()
-
-                if 'coches.net' in url:
-                        # print(soup)
-                        # <div class="mt-AnimationFadeOut mt-ListAds-item mt-CardAd mt-CardBasic">
-                        bloques = soup.find_all('div', class_="sui-AtomCard-info")
-                        try:
-                                assert len(bloques) > 0
-                                for car in bloques:
-                                        nombre_modelo = car.find('h2').text
-                                        precios = [precio.text for precio in car.find_all('h3')]
-                                        caracteristicas = [li.text for li in car.find_all('li')]
-                                        
-                                break
-                        except:
-                                print(f"{proxie} not worked for coches.net")
-
-                wd.quit()
  
                 
                 
