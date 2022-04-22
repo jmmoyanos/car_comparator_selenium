6b9ce7f91956a0356a5c879f74f12e1e928386e5
Jose maria Moyano <josemariamoyano@MacBook-Pro-de-Jose.local>
2022-04-22 13:25:10 +0200

    updated read.me

--- docker/secrets/google_drive.py
+++
@@ -1,9 +0,0 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from oauth2client.client import GoogleCredentials
-
-
-gauth = GoogleAuth()
-gauth.credentials = GoogleCredentials.get_application_default()
-drive = GoogleDrive()
-
--- docker/secrets/readme.md
+++
@@ -1,4 +0,0 @@
-
-```bash
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/josemariamoyano/Documents/python_projects/car_cormparator/src/secrets/client_secrets.json"
-```
